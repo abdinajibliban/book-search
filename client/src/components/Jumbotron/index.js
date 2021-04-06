874cad2c7f67aa8e27756c1187f7fed7bead6e32
Abdinajib Liban <abdinajibliban@gmail.com>
2021-04-06 12:54:49 -0400

    jumbotron component

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid bg-white">
+            <div className="container">
+                <h1 className="display-4 text-center">Google Books Search</h1>
+                <p className="lead text-center">Search for and Save Books of Interest</p>
+            </div>
+      </div>
+    )
+}
+
+export default Jumbotron
